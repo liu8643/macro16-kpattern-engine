df69f6f2ec8bf7955f7c7a738cf92f71353e1149
liu8643 <135194085+liu8643@users.noreply.github.com>
2026-05-06 17:39:56 +0800

    add kpattern pipeline core

---
+++ kpattern_module/pipeline.py
@@ -0,0 +1,41 @@
+from .feature_engine import KPatternFeatureEngine
+from .sakata_engine import SakataPatternEngine
+from .position_engine import PatternPositionEngine
+from .volume_engine import VolumeConfirmEngine
+from .score_engine import PatternScoreEngine
+
+
+class KPatternPipeline:
+    """
+    KPattern 主流程
+    負責串接所有子引擎
+    """
+
+    def __init__(self):
+        self.feature = KPatternFeatureEngine()
+        self.pattern = SakataPatternEngine()
+        self.position = PatternPositionEngine()
+        self.volume = VolumeConfirmEngine()
+        self.score = PatternScoreEngine()
+
+    def run(self, df):
+        """
+        主執行流程
+        """
+
+        # 1. K線特徵
+        df = self.feature.build(df)
+
+        # 2. 型態判斷
+        df = self.pattern.classify(df)
+
+        # 3. 位階判斷
+        df = self.position.build(df)
+
+        # 4. 量能判斷
+        df = self.volume.build(df)
+
+        # 5. 分數與方向
+        df = self.score.build(df)
+
+        return df
